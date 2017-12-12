134f7bd5c0ee2f3947a00e6bd2876af8131fdf53
jmil1206 <34387302+jmil1206@users.noreply.github.com>
2017-12-11 21:47:59 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+// Image switcher code
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/m-dot.jpg') {
+    myImage.setAttribute ('src', 'images/m-dot2.jpg');
+  } else {
+    myImage.setAttribute ('src', 'images/m-dot.jpg');
+  }
+}
+//Welcome Message code
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storeName = localStorage.getItem('name');
+  myHeading.innerHTML = storedName + 'Triathon finish times';
+}
+
+myButton.onclick = function () {
+  setUserName();
+}
