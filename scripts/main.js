e2c24ceb0dddeb5e5c43fa0ddf97983656cdb849
jmil1206 <34387302+jmil1206@users.noreply.github.com>
2017-12-11 21:55:41 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = myName + ' Triathlon Times';
 }
 if(!localStorage.getItem('name')) {
   setUserName();
